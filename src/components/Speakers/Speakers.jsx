e0214ecd821365bc2c5bf01065e96ecbb63d146f
david waza <davidyega69@gmail.com>
2025-01-24 09:41:13 +0100

    fix speakers

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,54 +1,9 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
 
 const Volunteers = ({ page }) => {
-  const speakerData = [
-    {
-      firstName: "David",
-      lastName: "Waza",
-      designation: 'Ceo Binary'
-    },
-    {
-      firstName: "Andra",
-      lastName: "Karagama",
-      designation: 'CTO Binary'
-
-    },
-    {
-      firstName: "Thaywo",
-      lastName: "Hassan",
-      designation: 'Lead Developer'
-
-    },
-  ];
-
-  return (
-    <section className="sec-pad-top sec-pad-bottom">
-      <Container>
-        <Row className="gutter-y-30">
-          <div className="container-fluid">
-            {speakerData.map(({ firstName, lastName, designation }, index) => (
-              <figure key={index}>
-                <div className="media figure-img-two"></div>
-                <figcaption>
-                  <div className="container-details">
-                    <div className="contact-details">
-                      <p>{firstName}</p>
-                      <p>{lastName}</p>
-                    </div>
-                    <div className="designation-details">
-                      <p>{designation}</p>
-                    </div>
-                  </div>
-                </figcaption>
-                <a href="#"></a>
-              </figure>
-            ))}
-          </div>
-        </Row>
-      </Container>
-    </section>
-  );
+ return (
+  <div className="speaker">No Speaker(s) available</div>
+ )
 };
 
 export default Volunteers;
